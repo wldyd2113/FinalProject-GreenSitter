e35fab246fb90f3c27636fa79bba27c8e1d01f96
jiyong <wldyd2113@gmail.com>
2025-04-05 19:17:25 +0900

    doc: AboutMe 파일 구조 변경

---
+++ GreenSitter/Profile/View/AboutMe/AboutMeViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  AboutMeViewModel.swift
+//  GreenSitter
+//
+//  Created by 차지용 on 4/5/25.
+//
+
+import Foundation
+import FirebaseAuth
+import FirebaseFirestore
+import FirebaseStorage
+import Combine
+
+class AboutMeViewModel: ObservableObject {
+    
+}
